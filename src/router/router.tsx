56c0efe6231be4ade63d8e456f71eb8989546cc8
Guillermo Merino <guillermo.merino@incotel.es>
2024-03-26 18:24:43 +0100

    refactor router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,10 +2,24 @@ import { Route, Routes } from 'react-router-dom'
 import PasswordGenerator from '../views/PasswordGenerator';
 import MyPasswords from '../views/MyPasswords';
 
+const _routes = [
+  {
+    path: "/",
+    element: <PasswordGenerator />
+  },
+  {
+    path: "/mypasswords",
+    element: <MyPasswords />
+  }
+]
+
 const AppRouter = () => (
   <Routes>
-    <Route path="/" element={<PasswordGenerator />} />
-    <Route path="/mypasswords" element={<MyPasswords />} />
+    {
+      _routes.map(({ path, element }) => (
+        <Route key={path} path={path} element={element} />
+      ))
+    }
   </Routes>
 );
 
