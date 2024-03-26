a5ec43fde26cbd6b5b6c2081ca588328af5ec4de
Guillermo Merino <guillermo.merino@incotel.es>
2024-03-26 17:36:32 +0100

    add menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,14 @@ import IconGithub from "./Icons/IconGithub"
 
 const Header = () => {
   return (
-    <header className='bg-black h-[35px] md:h-[50px] flex justify-end items-center px-4'>
+    <header className='bg-black h-[35px] md:h-[50px] flex justify-between items-center px-4'>
+      <nav>
+        <ul className="flex items-center gap-4 text-white">
+          <li className="menu-option">Password generator</li>
+          <li className="menu-option">My passwords</li>
+        </ul>
+      </nav>
+      
       <a
         className='text-2xl text-white'
         href='https://github.com/guillermo403/password-generator'
